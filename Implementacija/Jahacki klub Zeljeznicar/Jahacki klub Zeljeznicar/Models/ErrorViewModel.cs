fcdfb6f8eaaf39b1c13a8e8c4365a6f68f620016
VedadG <matrix662004@gmail.com>
2025-04-15 15:17:39 +0200

    Implementacija init

---
+++ Implementacija/Jahacki klub Zeljeznicar/Jahacki klub Zeljeznicar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jahacki_klub_Zeljeznicar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
